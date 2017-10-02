6b3d1b6e337baeda6018dee9cf6bac1ead89cb38
wilsonfisk <wilson.fisk.secure@gmail.com>
2017-10-02 14:27:49 -0400

    Split 'obfsc8.sh' into 2 steps.

--- obfsc8.sh
+++ obfsc8.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-pre="eval '"; post="'"
-text="command-to-hide"
-echo -n "$pre$(echo -n 'echo $(base64 -w0 <<< '"$text"') | base64 -di' | od -A n -t x1 | \
-	sed -e 's/[[:xdigit:]]\{2\}/\x&/g' -e 's/ //g' -e 's/\x/\\x/g' | tr -d '\n')$post" | \
-	rev | tr '\!-~' 'P-~\!-O'
+pre="eval $'"; post="'"
+text=$(base64 -w0<<<"command-to-hide")
+step1=$(echo -n "$pre";echo -n 'base64 -di <<<'"$text"|od -A n -t x1|sed -e 's/[[:xdigit:]]\{2\}/\x&/g' -e 's/ //g' -e 's/\x/\\x/g'|tr -d '\n';echo -n "$post")
+step2=$(echo "$step1"|rev|tr '\!-~' 'P-~\!-O')
+echo "$step2"
+
+# Reverse process:
+# tr 'P-~\!-O' '\!-~'
+# rev
