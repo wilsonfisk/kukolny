baacb5dde0e870e572a6784ec9a2edf61f67efb4
wilsonfisk <wilson.fisk.secure@gmail.com>
2017-10-08 00:51:38 -0400

    'obfsc8.sh' changed back to a single step. Also added a XOR function to 'obfsc8.sh'.

--- obfsc8.sh
+++ obfsc8.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+xor(){
+    local i
+    for ((i=0;i<${#1};i++));
+        do printf \\"$(printf '%03o' $(( $(printf '%d' "'${1:$i:1}") ^ 31 )) )";
+    done; printf "\n"
+}
 pre="eval $'"; post="'"
-text=$(base64 -w0<<<"command-to-hide")
-step1=$(echo -n "$pre";echo -n 'base64 -di <<<'"$text"|od -A n -t x1|sed -e 's/[[:xdigit:]]\{2\}/\x&/g' -e 's/ //g' -e 's/\x/\\x/g'|tr -d '\n';echo -n "$post")
-step2=$(echo "$step1"|rev|tr '\!-~' 'P-~\!-O')
-echo "$step2"
-
-# Reverse process:
-# tr 'P-~\!-O' '\!-~'
-# rev
+mid=$(printf "%s%s" 'base64 -di<<<' "$(base64 -w0 <<< $@)"|od -A n -t x1|\
+    sed -e 's/[[:xdigit:]]\{2\}/\x&/g' -e 's/ //g' -e 's/\x/\\x/g'|\
+    tr -d '\n')
+xor "$(echo -n "$pre$mid$post"|tr '\!-~' 'P-~\!-O'|rev)"
+printf '\n'
